06021de7fca19f96de7688178a387578ae652c19
tumbleW-eed <tumbleWeed_06@proton.me>
2023-03-07 21:39:59 +0530

    added playRound and player func

--- script.js
+++ script.js
@@ -2,13 +2,41 @@ let getComputerChoice = () => {
     let compVal = Math.random();
     console.log(compVal);
     if (compVal <= 0.33) {
-     return "Rock";
+     return "rock";
     } else if (compVal >= 0.33 && compVal <= 0.66) {
-     return "Paper"
+     return "paper"
     } else {
-     return "Scissors"
+     return "scissors"
     }
  };
  
 
- console.log(getComputerChoice());
+
+let playerSelection = () => choice = prompt();
+
+
+let play = (compChoice, playerChoice) => {
+
+    
+    console.log(playerChoice);
+    if (compChoice == playerChoice) {
+        return "Draw!"
+    } else if (compChoice == "rock" && playerChoice == "scissors" ) {
+        return "You Lose! Rock beats Scissors"
+    } else if (compChoice == "paper" && playerChoice == "rock" ) {
+        return "You Lose! Paper beats Rocks"
+    } else if (compChoice == "scissors" && playerChoice == "paper" ) {
+        return "You Lose! Scissors beats Paper"
+
+    } else if (playerChoice == "rock" && compChoice == "scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerChoice == "paper" && compChoice == "rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerChoice == "scissors" && compChoice == "paper") {
+        return "You Win! scissors beats paper"
+    } else {
+        return "something went wrong"
+    }
+}
+
+console.log(play(getComputerChoice(), playerSelection()));

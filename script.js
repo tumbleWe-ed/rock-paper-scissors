750354d6c7bd8490b343aff0aea8be8fb6519c60
tumbleW-eed <tumbleWeed_06@proton.me>
2023-03-09 21:17:38 +0530

    added multiple rounds

--- script.js
+++ script.js
@@ -15,28 +15,37 @@ let getComputerChoice = () => {
 let playerSelection = () => choice = prompt();
 
 
+
 let play = (compChoice, playerChoice) => {
 
-    
-    console.log(playerChoice);
     if (compChoice == playerChoice) {
         return "Draw!"
     } else if (compChoice == "rock" && playerChoice == "scissors" ) {
+        
         return "You Lose! Rock beats Scissors"
     } else if (compChoice == "paper" && playerChoice == "rock" ) {
+
         return "You Lose! Paper beats Rocks"
     } else if (compChoice == "scissors" && playerChoice == "paper" ) {
+
         return "You Lose! Scissors beats Paper"
 
     } else if (playerChoice == "rock" && compChoice == "scissors") {
+
         return "You Win! Rock beats Scissors"
     } else if (playerChoice == "paper" && compChoice == "rock") {
+
         return "You Win! Paper beats Rock"
     } else if (playerChoice == "scissors" && compChoice == "paper") {
+
         return "You Win! scissors beats paper"
     } else {
         return "something went wrong"
     }
+
 }
 
-console.log(play(getComputerChoice(), playerSelection()));
+for (let i = 0; i < 5; i++) {
+    console.log(play(getComputerChoice(), playerSelection()));
+}
+
